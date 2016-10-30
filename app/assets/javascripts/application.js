5c1771f785f132284805195cd68234b3143280d6
SiamKing <thesiameseelectric@gmail.com>
2016-10-30 09:44:08 -0700

    strap that boot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery
+
+$(document).ready(function() {
+
+});
